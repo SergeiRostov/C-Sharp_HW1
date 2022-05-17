c954909312136566fceb2df01e5831b539330e1d
Sergei Rostov <sergeitoday@gmail.com>
2022-05-17 12:50:36 +0300

    Add Lesson1-1

---
+++ Lesson1-1/Program.cs
@@ -0,0 +1,19 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.WriteLine("Введите число А: "); 
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B: "); 
+int b = int.Parse(Console.ReadLine());
+
+if(a == b*b)
+{
+    Console.WriteLine("Число А является квадратом числа В");
+}
+else
+{
+  Console.WriteLine("Число А НЕ является квадратом числа В");  
+}
+
+
+
