3fa6281f3e9354a9a2a721a6ccdec9c299d5b1fa
Sergei Rostov <sergeitoday@gmail.com>
2022-05-31 12:15:12 +0400

    Corrected string 12

---
+++ Lesson1-4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать четные числа от 1 до N
+
+Console.Write("Введите последнее число диапазона: ");
+int n = int.Parse(Console.ReadLine());
+int count = 2;
+if (n < 2)
+{
+    Console.WriteLine("Введен неверный диапазон!");
+}
+else
+{
+    Console.WriteLine("Четные числа от 1 до N:");
+    while (count <= n)
+    {
+        Console.Write($"{count} ");
+        count = count + 2;
+    }
+    Console.WriteLine();
+}
