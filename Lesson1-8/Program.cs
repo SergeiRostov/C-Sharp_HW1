e85589073f96b6067bc8b626b6ad0aab3445ba2f
Sergei Rostov <sergeitoday@gmail.com>
2022-05-31 15:36:19 +0400

    Add Lesson1-8

---
+++ Lesson1-8/Program.cs
@@ -0,0 +1,20 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Введите исходное число: ");
+int initialN = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите проверяемое число: ");
+int checkN = int.Parse(Console.ReadLine());
+
+    if (initialN >= checkN)
+        if (initialN % checkN == 0)
+        {
+            Console.WriteLine($"{checkN} кратно {initialN}");
+        }
+        else
+        {
+            Console.WriteLine($"{checkN} НЕ кратно {initialN}, остаток от деления {initialN % checkN}");
+        }
+    else 
+    {
+        float R = (float) initialN / checkN;
+        Console.WriteLine($"{checkN} НЕ кратно {initialN}, остаток от деления {R}");
+    }
