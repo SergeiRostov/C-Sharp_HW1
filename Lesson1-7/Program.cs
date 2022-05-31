0c221e305324f5ce30d4736dcb06dd55eea1dfb3
Sergei Rostov <sergeitoday@gmail.com>
2022-05-31 14:25:52 +0400

    Add Lesson1-7

---
+++ Lesson1-7/Program.cs
@@ -0,0 +1,8 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное значное число: ");
+int n = int.Parse(Console.ReadLine());
+int first = n / 100;
+int third = n % 10;
+Console.WriteLine($"Число без второй цифры: {first}{third}");
+
+
