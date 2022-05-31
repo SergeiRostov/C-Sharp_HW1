1bb6573a744a2575fbc6bbb86705c1171aaf5c6b
Sergei Rostov <sergeitoday@gmail.com>
2022-05-31 13:42:11 +0400

    Add Lesson1-5

---
+++ Lesson1-5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число: ");
+int n = int.Parse(Console.ReadLine());
+if (n > 99 && n < 1000)
+{
+    n = n / 10 % 10;
+    Console.WriteLine($"Вторая цифра этого числа: {n}");
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+
+
+
+
